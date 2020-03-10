6c1149b0e80834c7afbc8f9cc6efec8fdc823bf3
pollindrom@gmail.com <pollindrom@gmail.com>
2020-03-10 18:05:48 +0200

    gdf

---
+++ app/js/common.js
@@ -0,0 +1,617 @@
+$(document).ready(function () {
+   //////////////////////////////////////////////////////////////
+   ////// Services cards hover///////////////////////////////////
+
+   $('#serviceCard1').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard1').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard1 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard1 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard1 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard1 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard2').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard2').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard2 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard2 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard2 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard2 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard3').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard3').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard3 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard3 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard3 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard3 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard4').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard4').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard4 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard4 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard4 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard4 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard5').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard5').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard5 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard5 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard5 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard5 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard6').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard6').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard6 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard6 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard6 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard6 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard7').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard7').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard7 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard7 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard7 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard7 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard8').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard8').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard8 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard8 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard8 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard8 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+
+   $('#serviceCard9').mouseenter(function () {
+      $('.service-card').css('opacity', '.6');
+      $('#serviceCard9').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.47)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '56px',
+         'box-shadow': '0px 0px 2px $orange, 0px 0px 0px $orange, 0px 0px 10px $orange'
+      });
+
+      $('#serviceCard9 .service-description').css({
+         display: 'flex'
+      });
+
+      $('#serviceCard9 .icon-wrapper').css({
+         display: 'none'
+      });
+   }).mouseleave(function () {
+      $('.service-card').css('opacity', '1');
+      $('#servicesSectionTitle').css('color', 'rgba(255, 255, 255, 0.87)');
+      $('#servicesSectionTitleSpan').css({
+         'font-size': '54px',
+         'box-shadow': '0px 0px 0px $orange, 0px 0px 0px $orange, 0px 0px 0px $orange'
+      });
+
+      $('#serviceCard9 .service-description').css({
+         display: 'none'
+      });
+
+      $('#serviceCard9 .icon-wrapper').css({
+         display: 'flex'
+      });
+   });
+   /////////////////END////////////////////////
+
+   //////////////////////////////////////////////////////////////
+   ////// WE MAKE ANIMATION ///////////////////////////////////
+   let i = 1;
+   let leftPost = 0;
+   let offset = 300;
+
+   $('#rightArrow').mouseenter(function () {
+      let left = $('#weMakeCardsBlock').css('left');
+
+      $('#weMakeCardsBlock').animate({
+         'left': parseInt(left.substr(0, left.length - 2)) - 10 + 'px'
+      }, 100);
+
+      $(this).animate({
+         'margin-left': 10 + 'px'
+      }, 100);
+   }).mouseleave(function () {
+      let left = $('#weMakeCardsBlock').css('left');
+
+      $('#weMakeCardsBlock').animate({
+         'left': parseInt(left.substr(0, left.length - 2)) + 10 + 'px'
+      }, 100);
+
+      $(this).animate({
+         'margin-left': 0 + 'px'
+      }, 100);
+   });
+
+
+   $('#rightArrow').click(function () {
+      console.log(i);
+      if (i != 10) {
+         $('#wmCard1').animate({
+            'background-position-x': '-989px',
+            'opacity': 0
+         }, 400);
+
+         if (i == 1) {
+            $('#wmCard2').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+            setTimeout(function () {
+               $('#wmCard2').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+
+            $('#wmCard3').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard4').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 2) {
+            $('#wmCard2').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard3').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard3').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard4').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard5').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 3) {
+            $('#wmCard3').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard4').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard4').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard5').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard6').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 4) {
+            $('#wmCard4').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard5').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard5').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard6').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard7').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 5) {
+            $('#wmCard5').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard6').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard6').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard7').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard8').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 6) {
+            $('#wmCard6').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard7').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard7').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard8').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            $('#wmCard9').animate({
+               'background-position-x': '-1889px',
+            }, 400);
+         }
+
+         if (i == 7) {
+            $('#wmCard7').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard8').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard8').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+
+            $('#wmCard9').animate({
+               'background-position-x': '-1589px',
+            }, 400);
+
+            // $('#wmCard9').animate({
+            //    'background-position-x': '-1889px',
+            // }, 400);
+         }
+
+         if (i == 8) {
+            $('#wmCard8').animate({
+               'background-position-x': '-989px',
+               'opacity': 0
+            }, 400);
+
+            $('#wmCard9').animate({
+               'background-position-x': '-1289px',
+               'border': '2px solid #16F898',
+               'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+            }, 400);
+
+            setTimeout(function () {
+               $('#wmCard9').css({
+                  'border': '2px solid #16F898',
+                  'box-shadow': '0px 0px 2px #16F898, 0px 0px 10px #16F898'
+               });
+            }, 300);
+         }
+
+         if (i < 10) {
+            $('#weMakeCardsBlock').animate({
+               'left': leftPost - offset + 'px'
+            }, 400);
+
+            i++;
+            leftPost = leftPost - offset;
+         }
+
+
+         if (i == 10) {
+            $('#wmCard9').animate({
+               'opacity': 0
+            }, 400);
+
+            $('#weMakeCardsBlock').animate({
+               'left': 0
+            }, 10);
+
+            $('#wmCard1').animate({
+               'background-position-x': '-1289px',
+            }, 100);
+
+            $('#wmCard2').animate({
+               'background-position-x': '-1589px',
+            }, 100);
+
+            $('#wmCard3').animate({
+               'background-position-x': '-1889px',
+            }, 100);
+
+
+            setTimeout(function () {
+               for (var j = 1; j < 10; j++) {
+                  $('#wmCard' + j).css({
+                     'opacity': 1,
+                  });
+                  if (j != 1) {
+                     $('#wmCard' + j).css({
+                        'border': '0px',
+                        'box-shadow': 'none'
+                     });
+                  }
+               }
+            }, 600)
+
+
+            i = 1;
+            leftPost = 0;
+         }
+      }
+
+   });
+   /////////////////END////////////////////////
+
+   //////////////////////////////////////////////////////////////
+   ////// Burger mnu ///////////////////////////////////
+
+   $('#burger').click(function () {
+      if($('#mobileMnu').css('left') == '0px') {
+         $('#mobileMnu').animate({'left': '-100vw'}, 300)
+      } else {
+         $('#mobileMnu').animate({'left': '0'}, 300)
+      }
+      
+   });
+});
